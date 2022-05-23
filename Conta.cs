4d713dd3d4c2e9c727b7163ab58aa7bd1a2ab4f9
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2022-05-22 23:19:17 -0300

    Mostrando opções.

--- Conta.cs
+++ Conta.cs
@@ -13,12 +13,35 @@ namespace BankApp
         public string Nome { get; set; }
         public decimal Saldo { get; set; }
         public decimal Credito { get; set; }
+        public decimal Taxa { get; set; }
 
         public Conta(string nome)
         {
             Random rand = new Random();
             Codigo = rand.Next(1000, 9999).ToString();
             Nome = nome;
+        }
+
+        public decimal Depositar(decimal quantidade)
+        {
+            Saldo += quantidade;
+            return Saldo;
+        }
+
+        public decimal Sacar(decimal quantidade)
+        {
+            Saldo -= quantidade - (Saldo * (Taxa / 100));
+            return Saldo;
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine(
+                "Código da Conta: "+Codigo+
+                "\nNome: " +Nome+
+                "\nSaldo: R$"+Saldo+
+                "\nCrédito: R$"+Credito
+            );
         }
 
     }
--- Principal.cs
+++ Principal.cs
@@ -7,6 +7,43 @@ namespace BankApp
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Bem-vindo ao caixa eletrônico!");
+            Console.WriteLine("------------------------------\n");
+
+            bool Lifecycle1 = true;
+
+            while (Lifecycle1)
+            {
+                Console.WriteLine("Escolha um tipo de conta para começar:" +
+                    "\n(1) Conta corrente" +
+                    "\n(2) Conta poupança" +
+                    "\n(3) Conta especial" +
+                    "\n(4) Cancelar");
+
+                string TipoDeConta = Console.ReadLine();
+
+                switch (TipoDeConta)
+                {
+                    case "1":
+                        break;
+
+                    case "2":
+                        break;
+
+                    case "3":
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Encerrando aplicação...");
+                        Lifecycle1 = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Tipo de conta inválido. Digite o número da opção desejada.");
+                        break;
+                }
+            }
+
         }
     }
 }
